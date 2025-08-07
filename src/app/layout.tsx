476932e968beac26e5fe12956ebbf60aef05bbee
ArtoriasForset <artoriase@outlook.com>
2025-08-07 18:19:23 +0800

    8/7日

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
             </Link>
 
             <div className="relative group p-4 transform hover:bg-gray-200 transition z-10">
-              <Link href="/task" className="p-4 font-semibold">首页</Link>
+              <Link href="/task" className="p-4 font-semibold hover:underline underline-offset-8">首页</Link>
               <div className="absolute absolute left-0 top-full bg-gray-200 invisible group-hover:visible opacity-0 group-hover:opacity-100">
                 <Link href="/task" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
                   宣传页面
@@ -38,7 +38,7 @@ export default function RootLayout({
             </div>
 
             <div className="relative group p-4 transform hover:bg-gray-200 transition w-32 z-10">
-              <Link href="/task" className="p-4 font-semibold">公司简介</Link>
+              <Link href="/task" className="p-4 font-semibold hover:underline underline-offset-8">公司简介</Link>
               <div className="absolute absolute w-32 text-center left-0 top-full bg-gray-200 invisible group-hover:visible opacity-0 group-hover:opacity-100">
                 <Link href="/task" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
                   企业资质
@@ -46,14 +46,11 @@ export default function RootLayout({
                 <Link href="/task/create" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
                   企业文化
                 </Link>
-                <Link href="/task/create" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
-                  公司新闻
-                </Link>
               </div>
             </div>
 
             <div className="relative group p-4 transform hover:bg-gray-200 transition w-32 z-10">
-              <Link href="/task" className="p-4 font-semibold">办公地址</Link>
+              <Link href="/task" className="p-4 font-semibold hover:underline underline-offset-8">办公地址</Link>
               <div className="absolute text-center absolute left-0 w-32 top-full bg-gray-200 invisible group-hover:visible opacity-0 group-hover:opacity-100">
                 <Link href="/task" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
                   全国布局图
@@ -62,7 +59,22 @@ export default function RootLayout({
             </div>
 
             <div className="relative group p-4 transform hover:bg-gray-200 transition w-36 z-10">
-              <Link href="/task" className="p-4 font-semibold">客户端下载</Link>
+              <Link href="/task" className="p-4 font-semibold hover:underline underline-offset-8">产品与服务</Link>
+              <div className="absolute text-center w-36 absolute left-0 top-full bg-gray-200 invisible group-hover:visible opacity-0 group-hover:opacity-100">
+                <Link href="/task" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
+                  共享办公室
+                </Link>
+                <Link href="/task/create" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
+                  虚拟办公室
+                </Link>
+                <Link href="/task" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
+                  定制办公室
+                </Link>
+              </div>
+            </div>
+
+            <div className="relative group p-4 transform hover:bg-gray-200 transition w-36 z-10">
+              <Link href="/task" className="p-4 font-semibold hover:underline underline-offset-8">客户端下载</Link>
               <div className="absolute text-center w-36 absolute left-0 top-full bg-gray-200 invisible group-hover:visible opacity-0 group-hover:opacity-100">
                 <Link href="/task" className="block p-4 text-gray-700 hover:bg-gray-100 hover:text-red-500">
                   安卓下载
@@ -74,7 +86,7 @@ export default function RootLayout({
                   电脑端下载
                 </Link>
               </div>
-            </div>
+            </div> 
 
             <div className="ml-auto flex items-center space-x-4 m-4 h-4">
               <p>联系电话 </p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
 <>  
-<div className="w-full h-[50vh] relative z-0">
+<div className="w-full h-[60vh] relative z-0">
   <Image
     src="/beijing.jpg"
     alt="首页主图"
@@ -14,13 +14,11 @@ export default function Home() {
 
   <div className="absolute inset-0 flex items-center justify-center z-10">
     <div className="text-center">
-      <h1 className="text-4xl font-bold mb-4 drop-shadow-lg bg-gray-50 p-2">区块链孵化器数字化管理平台</h1>
-      <p className="text-lg drop-shadow text-white bg-red-500 w-24 transform hover:scale-110 transition">点击了解</p>
+      <h1 className="text-4xl font-bold mb-4 drop-shadow-lg bg-gray-50 p-2">数智化产业·创孵云社区</h1>
+      <button className="text-xl drop-shadow text-white bg-red-500 w-24 transform hover:scale-110 transition">点击了解</button>
     </div>
   </div>
 </div>
-
-<div className="m-12 p-2 bg-gray-200">测试数据</div>
 
 </>
   );
